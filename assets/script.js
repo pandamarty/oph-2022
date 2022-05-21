a73cd8bd24c81c910d1641446a421e800faca7cf
Martina Burlando <martina.b@rrsolutions.it>
2022-05-21 11:36:47 +0200

    events display correctly - raw

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,21 @@ $.getJSON(
   "https://tourism.opendatahub.bz.it/v1/EventShort?pagenumber=1&sortorder=ASC&optimizedates=false&removenullvalues=false",
   function (data) {
     var itemList = data.Items;
-
-    var items = {
-      id: itemList.Id,
-      room: itemList.AnchorVenueRoomMapping,
-      company: itemList.CompanyName,
-      eDesc: itemList.EventDescription,
-      startDt: itemList.StartDate,
-      endDt: itemList.EndDate,
-    };
-
-    var source = document.getElementById("events-template").innerHTML;
-    var template = Handlebars.compile(source);
+    var content = document.getElementById("cards");
+    debugger;
+    for (var i = 0; i < itemList.length; i++) {
+      var description = itemList[i].EventDescription;
+      var correctedDes = description.toUpperCase();
+      var card = `<div class="column">
+                <div class="card" style="width: 18rem; height: auto">
+                            <div class="card-body">
+                                <h5 class="card-title">${correctedDes}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${itemList[i].CompanyName}</h6>
+                                <p class="card-text">${itemList[i].AnchorVenueRoomMapping}</p>
+                            </div>
+                        </div>
+                        </div>`;
+      content.innerHTML += card;
+    }
   }
 );
