9cbe8dbe68ed45b200782f10685725a01fcd8001
Martina Burlando <martina.b@rrsolutions.it>
2022-05-21 07:58:01 +0200

    added event visualization functionality

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 $.getJSON(
   "https://tourism.opendatahub.bz.it/v1/EventShort?pagenumber=1&sortorder=ASC&optimizedates=false&removenullvalues=false",
   function (data) {
-    console.log(data);
+    var itemList = data.Items;
+
+    var items = {
+      id: itemList.Id,
+      room: itemList.AnchorVenueRoomMapping,
+      company: itemList.CompanyName,
+      eDesc: itemList.EventDescription,
+      startDt: itemList.StartDate,
+      endDt: itemList.EndDate,
+    };
+
+    var source = document.getElementById("events-template").innerHTML;
+    var template = Handlebars.compile(source);
   }
 );
